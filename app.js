5606e56513af28af430798221b3c883bb5471338
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-05-23 13:19:43 -0700

    Added light switches.

--- app.js
+++ app.js
@@ -4,35 +4,64 @@ var fs = require('fs');
 // TODO: have this be soft coded.
 var PORT = 4000;
 
-var bcpm = require('./bcpm');
-var devices = [];
+var devices = require('./devices.json');
 var arr = [];
 
-setInterval(function () {
+(function loop() {
   arr = [];
-  bcpm = bcpm.map(function (device) {
+  devices.bcpm = devices.bcpm.map(function (device) {
     var status = Math.random() * 0.02;
     device.kw.Status = status.toString();
     device.kwh.Status = (parseFloat(device.kwh.Status) + status).toString();
     return device;
   });
-  bcpm.forEach(function (device) {
+  devices.bcpm.forEach(function (device) {
     arr.push(device.kw);
     arr.push(device.kwh);
   });
-  devices = arr;
+  devices.light_switches.forEach(function (device) {
+    arr.push(device);
+  });
 
-  fs.writeFile('bcpm.json', JSON.stringify(bcpm), 'utf8');
-}, 1000);
+  fs.writeFile('devices.json', JSON.stringify(devices), 'utf8');
+
+  setTimeout(function () { loop(); }, 1000);
+})();
 
 var app = express();
+
+app.use(express.json());
 
 app.get('/mControl/api/devices', function (req, res) {
   res.json(arr);
 });
 
+app.get('/mControl/api/devices/:id', function (req, res) {
+  var id = parseInt(req.params.id, 10);
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].Id === id) {
+      return res.json(arr[i]);
+    }
+  }
+  res.send(404, 'Device not found.');
+});
+
 app.put('/mControl/api/devices/:id/send_command', function (req, res) {
-  res.send(501, 'Coming soon');
+  var id = parseInt(req.params.id, 10);
+  var existed = arr.some(function (device) {
+    if (device.Id === id) {
+      if (req.body.command === 'on') {
+        device.Status = '100';
+      } else if (req.body.command === 'off') {
+        device.Status = '0';
+      }
+      return true;
+    }
+  });
+  if (!existed) {
+    return res.send(404, 'Device not found.');
+  }
+  res.send('Success!')
 });
 
 app.listen(PORT);
--- generators/devices.js
+++ generators/devices.js
@@ -1,3 +1,4 @@
+// Generates an object with two BCPM data devices: kw, kwh.
 function bcpm(id) {
   if (typeof id != 'number') {
     throw new Error('id must be a number');
@@ -38,10 +39,40 @@ function bcpm(id) {
   };
 }
 
-var count = parseInt(process.env.COUNT || 5, 10)
-var arr = [];
-for (var i = 0; i < 42; i++) {
-  arr.push(bcpm(i + 1));
+// Generates a light switch INSTEON device.
+function lightSwitch(id) {
+  if (typeof id != 'number') {
+    throw new Error('id must be a number');
+  }
+  var idstr = id.toString();
+  if (idstr.length < 2) { idstr = '0' + idstr; }
+  return {
+    "Id":id,
+    "Name":"switch_" + id,
+    "Order":0,
+    "Adapter":"2414x",
+    "Address":"18:18:" + id,
+    "Image":"Lamp",
+    "Module":"INST_BINSW",
+    "NotifyFlags":32767,
+    "Options":"",
+    "Protocols":"INSTEON",
+    "Status":"0",
+    "Visible":true,
+    "ZoneID":0
+  }
 }
 
-process.stdout.write(JSON.stringify(arr));
+var output = { bcpm: [], light_switches: [] };
+for (var i = 0; i < 42; i++) {
+  output.bcpm.push(bcpm(i + 1));
+}
+
+i = i*2 + 1;
+var max = i + 8;
+
+for (; i < max; i++) {
+  output.light_switches.push(lightSwitch(i));
+}
+
+process.stdout.write(JSON.stringify(output));
